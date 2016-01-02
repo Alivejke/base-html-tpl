915e4541e706c3d53a3049ca72e49ca9b746c510
note <note>
2016-01-02 13:59:53 +0100

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,142 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    protocol: 'http',
+                    hostname: 'localhost',
+                    base: 'build/'
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            jsVendor: {
+                src: [
+                    // 'bower_components/somelib/lib.js',
+                    'src/js/vendor/**/*'
+                ],
+                dest: 'build/js/vendor.js'
+            },
+            js: {
+                src: ['src/js/**/*.js', '!src/js/vendor/**/*'],
+                dest: 'build/js/main.js'
+            },
+            css: {
+                src: [
+                    // 'bower_components/somelib/lib.css',
+                    'tmp/css/main.css'
+                ],
+                dest: 'build/css/main.css'
+            }
+        },
+
+        less: {
+            dev: {
+                files: {
+                    'tmp/css/main.css': 'src/less/main.less'
+                }
+            },
+        },
+
+        copy: {
+            html: {
+                expand: true,
+                cwd: 'src/',
+                src: ['*.html'],
+                dest: 'build/'
+            },
+            img: {
+                expand: true,
+                cwd: 'src/img/',
+                src: ['**/*'],
+                dest: 'build/img/'
+            },
+            images: {
+                expand: true,
+                cwd: 'src/images/',
+                src: ['**/*'],
+                dest: 'build/images/'
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true
+            },
+
+            js: {
+                files: ['src/js/**/*.js'],
+                tasks: ['concat:js'],
+                options: {
+                    interrupt: true
+                }
+            },
+
+            jsVendor: {
+                files: ['src/js/vendor/**/*.js'],
+                tasks: ['copy:jsVendor'],
+                options: {
+                    interrupt: true
+                }
+            },
+
+            less: {
+                files: ['src/**/*.scss'],
+                tasks: ['less', 'concat:css'],
+                options: {
+                    interrupt: true
+                }
+            },
+
+            html: {
+                files: ['src/**/*.html'],
+                tasks: ['copy:html'],
+                options: {
+                    interrupt: true
+                }
+            },
+
+            img: {
+                files: ['src/img/**/*'],
+                tasks: ['copy:img'],
+                options: {
+                    interrupt: true
+                }
+            },
+
+            images: {
+                files: ['src/images/**/*'],
+                tasks: ['copy:images'],
+                options: {
+                    interrupt: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', [
+        'less',
+        'concat',
+        'copy'
+    ]);
+    grunt.registerTask('dev', [
+        'connect',
+        'watch'
+    ]);
+    grunt.registerTask('default', [
+        'build',
+        'dev'
+    ]);
+};
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# To interact with source code:
+1. Install nodejs - https://nodejs.org/
+2. Run `npm install grunt-cli -g` in console
+3. Go to projects folder in console and run `npm install`
+4. Go to projects folder in console and run `bower install`
+
+# To run development bundle:
+1. Go to projects folder in console and run `grunt`
+2. Go to `localhost:8000` in browser
+
+# To build the project:
+1. Go to projects folder in console and run `grunt build`
